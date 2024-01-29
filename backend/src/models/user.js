4ec199ffac9443e5bdc209452aeaa045317a26f1
James McConnell <interneth3ro@gmail.com>
2024-01-29 16:26:20 -0600

    added tokens prop to user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   id: { type: String },
+  tokens: { type: Number },
 });
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model('User', userSchema);
