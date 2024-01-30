d91159ec6afb7c6c1754d3bbeff80d234181d366
James McConnell <interneth3ro@gmail.com>
2024-01-29 18:41:07 -0600

    implemented coin flip

--- backend/src/api/coin-toss.js
+++ backend/src/api/coin-toss.js
@@ -10,7 +10,19 @@ const coinToss = async (req, res) => {
       return res.status(404).json({ message: 'User Does Not Exist' });
     }
 
-    res.status(200).json({ status: 'success' });
+    let isWin;
+    const rand = Math.floor(Math.random() * 100 + 1);
+    const isEven = rand % 2 === 0;
+    // heads is even, tails is odd
+    if (choice === 'heads' && isEven) {
+      isWin = true;
+    } else if (choice === 'tails' && !isEven) {
+      isWin = true;
+    } else {
+      isWin = false;
+    }
+
+    res.status(200).json({ result: isWin });
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong' });
   }
--- frontend/src/actions/game.js
+++ frontend/src/actions/game.js
@@ -5,6 +5,7 @@ import * as messages from '../messages';
 export const coinToss = (formData) => async (dispatch) => {
   try {
     const { data } = await api.coinToss(formData);
+    console.log(data);
     dispatch({ type: COINTOSS, data });
     messages.success('Coin Toss Successful!');
   } catch (error) {
